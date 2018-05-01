57e8a917e613c800d00a2bc88944432c130c9c37
lula <nnubeluz-20@hotmail.com>
2018-05-01 17:57:13 -0500

    Los cambios que hice2

--- Aplicativo/server.py
+++ Aplicativo/server.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, session
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask import request
@@ -14,20 +14,23 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = dbdir
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
+app.secret_key = 'esto-es-una-clave-muy-secreta'
 
 
 class Users(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	username = db.Column(db.String(30), unique=True, nullable=False)
 	password = db.Column(db.String(80), nullable=False)
-
+	
 
 @app.route('/', methods=["GET", "POST"])
 def login():
 	if request.method == "POST":
 		user = Users.query.filter_by(username=request.form["username"]).first()
 		if user and check_password_hash(user.password, request.form["password"]):
-			return redirect("http://127.0.0.1:5000/dashboard")
+			session['logueado'] = "si"
+			session['userlogueado'] = request.form["username"]
+			return redirect("/dashboard")
 		return "Tus credenciales son invalidas,revisa e intenta nuevamente"
 	return render_template('login.html')
 
@@ -43,11 +46,16 @@ def signup():
 
 @app.route('/dashboard')
 def dashboard():
-	return render_template("index.html")
+	if session:
+		return render_template("index.html")
+	return redirect("/error")
+
+@app.route('/error')
+def error():
+	return render_template("error.html")
+
 
 	
 if __name__=='__main__':
 	db.create_all()
-	app.run(debug=True)
-
-
+	app.run(debug=True, host='0.0.0.0')
